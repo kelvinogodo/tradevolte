1726e4df44f6f52a3a3f5b67fc7aeb9011e958ae
kelvinogodo <kelvinchukwuebuka385@gmail.com>
2023-03-14 03:49:25 -0700

    updated login error message

--- server.js
+++ server.js
@@ -341,14 +341,13 @@ app.post('/api/login', async (req, res) => {
       await User.updateOne({email: user.email},{$set:{rememberme : req.body.rememberme}})
       return res.json({ status: 'ok', user: token })
     }
-    
+    else if(user.password !== req.body.password){
+      return res.json({ status: 404, })
+    }
     else{
       return res.json({ status: 400 })
     }
-  }
-  else if(user.password !== req.body.password){
-      return res.json({ status: 404, })
-    } else {
+  } else {
     return res.json({ status: 'error', user: false })
   }
 })
